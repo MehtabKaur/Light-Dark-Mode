5df5decb900d19ff379dfe042c120f1bb5c9a5d8
Mehtab Kaur <kaur2shine@gmail.com>
2022-08-11 17:42:04 -0400

    implemented functionality

---
+++ script.js
@@ -0,0 +1,62 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// Dark or Light images
+function imageMode(color) {
+    // pass in light or dark mode for image color
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+// Dark Mode Styles
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'; // black, 50% opacity
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)'; // white, 50% opacity
+    // change toggle-text (light mode to dark mode)
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    // replace icon (sun to moon)
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    // images for dark mode
+    imageMode('dark');
+}
+
+
+// Light Mode Styles
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'; // white, 50% opacity
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)'; // black, 50% opacity
+    // change toggle-text (to light mode)
+    toggleIcon.children[0].textContent = 'Light Mode';
+    // replace icon (moon to sun)
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    // images for light mode
+    imageMode('light');
+
+}
+
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    // when boolean is checked (True)  - enable dark mode
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
+    }
+    // when boolean is unchecked (False) - enable light mode
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
+    }
+}
+
+// unchecked - light mode
+// checked - dark mode
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
